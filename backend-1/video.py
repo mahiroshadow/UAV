1e167adf4769fd96dea41817daa69678cf5ac5cf
mahiroshadow <treyarch827@gmail.com>
2025-02-10 22:02:55 +0800

    bug fix

---
+++ backend-1/video.py
@@ -0,0 +1,21 @@
+import cv2
+import subprocess
+from ultralytics import YOLO
+
+
+src = "rtmp://192.168.5.5:1935/live/home"
+dest = "rtmp://47.98.33.192:1935/live/test"
+command = [
+    'ffmpeg', '-y', '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt',
+    'bgr24', '-s', "{}x{}".format(720, 1280), '-r',
+    str(30), '-i', '-', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-preset',
+    'ultrafast', '-f', 'flv', dest
+]
+model = YOLO("yolo11n.pt").cuda()
+cap = cv2.VideoCapture(src)
+pipe = subprocess.Popen(command, shell=False, stdin=subprocess.PIPE)
+while True:
+    _, frame = cap.read()
+    res = model(frame)
+    pipe.stdin.write(frame)
+    cv2.waitKey(1)
